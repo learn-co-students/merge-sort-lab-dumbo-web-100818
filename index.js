7ec325ab545c993c09f718a1fbfbea3fb46b3252
hmalik88 <hbmalik88@gmail.com>
2019-02-21 00:42:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function findMinAndRemoveSorted(arr) {
+  return arr.shift();
+}
 
+function merge(arr1, arr2) {
+  let arr = [];
+  while (arr1.length !== 0 && arr2.length !== 0) {
+    if (arr1[0] < arr2[0]) {
+      arr.push(arr1.shift())
+    } else {
+      arr.push(arr2.shift())
+    }
+  }
+  return arr.concat(arr1).concat(arr2);
+}
+
+function mergeSort(arr) {
+  let mid = arr.length/2
+  let firstHalf = arr.slice(0, mid)
+  let secondHalf = arr.slice(mid, arr.length)
+  if (arr.length < 2) {
+    return arr
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
