475c49f6a07c66da429ed6eecfc2d15b36f45618
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 23:36:44 +0000

    create mergeSort function to sort an unsorted array and pass the tests

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 
+function findMinAndRemoveSorted(array) {
+  let minIdx = 0
+  let min = array[0]
+  for(let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      minIdx = i
+      min = array[i]
+    }
+  }
+  array.splice(minIdx, 1)
+  return min
+}
+
+function merge(firstHalf, secondHalf){
+  let sorted = []
+  while(firstHalf.length !== 0 && secondHalf.length !== 0) {
+    if (firstHalf[0] < secondHalf[0]) {
+      let currentMin = findMinAndRemoveSorted(firstHalf)
+      sorted.push(currentMin)
+    } else {
+      let currentMin = findMinAndRemoveSorted(secondHalf)
+      sorted.push(currentMin)
+    }
+  }
+  return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array) {
+  let midPoint = array.length/2
+  let firstHalf = array.slice(0, midPoint)
+  let secondHalf = array.slice(midPoint, array.length)
+  
+  if (array.length < 2) {
+    return array
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
